32c0ea936cd3a9a4e21c4873aebd19d59d28bada
ma-wais <mawais20212021@gmail.com>
2024-10-06 12:33:44 +0500

    server

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,8 @@ import EditEmployee from "./pages/EditEmployee.jsx";
 import ManageEmployeesPage from "./pages/ManageEmployee.jsx";
 // import AdminLogin from "./pages/AdminLogin.jsx";
 
-export const server = "http://localhost:5000/api";
-// export const server = "https://hotel-memt-plus-att-api.onrender.com/api"
+// export const server = "http://localhost:5000/api";
+export const server = "https://hotel-memt-plus-att-api.onrender.com/api"
 
 function App() {
   return (
